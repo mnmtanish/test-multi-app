ff2fb163b8f006cf8dda4d6408e6037743f7ec1c
Muhammed Thanish <mnmtanish@gmail.com>
2017-06-21 13:51:18 +0530

    Open and close tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public allDiagrams: string[];
+  public openDiagrams: string[];
+
+  constructor() {
+    this.allDiagrams = [ 'Diagram 1', 'Diagram 2', 'Diagram 3' ];
+    this.openDiagrams = [];
+  }
+
+  public open(name: string) {
+    const index = this.openDiagrams.indexOf(name);
+    if (index === -1) {
+      this.openDiagrams.push(name);
+    }
+  }
+
+  public close(name: string) {
+    const index = this.openDiagrams.indexOf(name);
+    if (index !== -1) {
+      this.openDiagrams.splice(index, 1);
+    }
+  }
 }
