053a8acc4918e6587453c04069d99ad652945f7f
Muhammed Thanish <mnmtanish@gmail.com>
2017-07-03 13:00:16 +0530

    Refactor npm script 'app'

--- .npm/app.js
+++ .npm/app.js
@@ -2,11 +2,13 @@ const { exec } = require('shelljs');
 const chokidar = require('chokidar');
 
 const build = () => exec('npm run build');
-build();
+const server = () => exec('http-server ./dist -p 4200', { async: true });
+const electron = () => exec('electron .', { async: true });
 
 chokidar
     .watch('src', { ignoreInitial: true })
     .on('all', () => build());
 
-const server = exec('http-server ./dist -p 4200', { async: true });
-const electron = exec('electron .', { async: true });
+build();
+server();
+electron();
